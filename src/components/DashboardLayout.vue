ac6c5c8732a40d54667783e06c6dbed2da061df7
Javier Munoz <javier@greicodex.com>
2025-11-12 08:20:52 -0400

    fixed side menu layout

--- src/components/DashboardLayout.vue
+++ src/components/DashboardLayout.vue
@@ -16,20 +16,22 @@ function logout() {
     <div class="flex h-screen bg-gray-900 text-white">
         <aside class="w-64 flex-shrink-0 bg-gray-800 p-6">
             <div class="mb-8 text-center">
-                <img alt="Wdlnds logo" class="inline-block" src="@/assets/logo.svg" width="150" />
+                <img alt="Wdlnds logo" class="inline-block wdlnds-logo" src="@/assets/logo.svg" width="150" />
             </div>
-            <nav class="flex flex-col space-y-2">
-                <RouterLink to="/" class="px-4 py-2 rounded-lg hover:bg-gray-700">Dashboard</RouterLink>
-                <RouterLink to="/api-tokens" class="px-4 py-2 rounded-lg hover:bg-gray-700">API Tokens</RouterLink>
-                <RouterLink to="/posts" class="px-4 py-2 rounded-lg hover:bg-gray-700">Posts</RouterLink>
-                <RouterLink to="/clients-hubs" class="px-4 py-2 rounded-lg hover:bg-gray-700">Clients & Hubs
+            <nav class="flex flex-col space-y-8 text-center ">
+                <RouterLink to="/" class="menu_item px-4 py-4 rounded-lg hover:bg-gray-700">Dashboard</RouterLink>
+                <RouterLink to="/api-tokens" class="menu_item px-4 py-4 rounded-lg hover:bg-gray-700">API Tokens
                 </RouterLink>
-                <RouterLink to="/webhooks" class="px-4 py-2 rounded-lg hover:bg-gray-700">Webhooks</RouterLink>
-                <RouterLink to="/about" class="px-4 py-2 rounded-lg hover:bg-gray-700">About</RouterLink>
+                <RouterLink to="/posts" class="menu_item px-4 py-4 rounded-lg hover:bg-gray-700">Posts</RouterLink>
+                <RouterLink to="/clients-hubs" class="menu_item px-4 py-4 rounded-lg hover:bg-gray-700">Clients & Hubs
+                </RouterLink>
+                <RouterLink to="/webhooks" class="menu_item px-4 py-4 rounded-lg hover:bg-gray-700">Webhooks
+                </RouterLink>
+                <RouterLink to="/about" class="menu_item px-4 py-4 rounded-lg hover:bg-gray-700">About</RouterLink>
             </nav>
-            <div class="mt-auto">
+            <div class="mt-auto " style="bottom: calc(var(--spacing) * 6);position: fixed;width: 205px;">
                 <a @click="logout" href="#"
-                    class="block w-full px-4 py-2 text-center bg-red-600 rounded-lg hover:bg-red-700">Logout</a>
+                    class="menu_item logout block w-full px-4 py-2 text-center bg-gray-600 rounded-lg hover:bg-orange-700">Logout</a>
             </div>
         </aside>
         <main class="flex-1 p-10 overflow-y-auto">
@@ -37,3 +39,18 @@ function logout() {
         </main>
     </div>
 </template>
+<style lang="css">
+.menu_item {
+    margin-top: 2em;
+    padding-top: .5em;
+    padding-bottom: .5em;
+}
+
+.logout {
+    vertical-align: bottom;
+}
+
+.wdlnds-logo {
+    filter: invert(1);
+}
+</style>
